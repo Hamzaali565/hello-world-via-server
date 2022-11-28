26afa77c18a59bccc539013d66f54b54b73cf5fe
Hamzaali565 <ha6153806@gmail.com>
2022-11-28 14:46:38 +0500

    static hosting

--- module.mjs
+++ module.mjs
@@ -1,11 +1,15 @@
 import express from "express"
+import path from 'path'
 const app = express()
 const port = process.env.PORT||3000
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
   res.send('Hello World!')
   console.log("request IP: ", req.ip );
 })
+const __dirname = path.resolve();
+app.use('/', express.static(path.join (__dirname, './web/build')))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
