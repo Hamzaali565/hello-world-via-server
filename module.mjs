64faf2df9813ac8a951f69d92aea12cf8046922a
Hamzaali565 <ha6153806@gmail.com>
2022-11-28 15:28:09 +0500

    single page updates

--- module.mjs
+++ module.mjs
@@ -9,6 +9,7 @@ app.get('/abc', (req, res) => {
 })
 const __dirname = path.resolve();
 app.use('/', express.static(path.join (__dirname, './web/build')))
+app.use('*', express.static(path.join (__dirname, './web/build')))
 
 
 app.listen(port, () => {
