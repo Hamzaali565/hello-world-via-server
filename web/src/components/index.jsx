b6763a7f1f9547f013e7ad0110b91029cf2befb4
Hamzaali565 <ha6153806@gmail.com>
2022-11-28 19:16:17 +0500

    working

--- web/src/components/index.jsx
+++ web/src/components/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react"
 import axios from "axios"
+import './index.css'
 import { useEffect } from "react";
 const Weather = () => {
     const [weather, setWeather] = useState([]);
@@ -29,20 +30,20 @@ const Weather = () => {
             <h1 className="h1"> React Weather App</h1>
                 <div className="mainGlass">
                     <div className="glass">
-                        <div className="city">{weather.data.name}</div>
+                        {/* <div className="city">{weather.data.name}</div> */}
                         <div className="opposite">
                             <div className="TMM">
 
-                                <div><span> Tempurature: </span>{Math.round(weather.data.temp)} °C</div>
-                                <div><span>Min-Temp:</span> {Math.round(weather.data.temp_min)} °C </div>
-                                <div><span>Max-Temp:</span> {Math.round(weather.data.temp_max)} °C</div>
+                                <div><span> Tempurature: </span>{Math.round(weather.temp)} °C</div>
+                                <div><span>Min-Temp:</span> {Math.round(weather.temp_min)} °C </div>
+                                <div><span>Max-Temp:</span> {Math.round(weather.temp_max)} °C</div>
 
                             </div>
 
                             <div className="HP">
 
-                                <div><span>Humidity:</span> {weather.data.humidity}%</div>
-                                <div><span>Pressure:</span> {weather.data.pressure}mb</div>
+                                <div><span>Humidity:</span> {weather.humidity}%</div>
+                                <div><span>Pressure:</span> {weather.pressure}mb</div>
 
                             </div>
                         </div>
