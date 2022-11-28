573c5b47e26ffb363fe05bc38789fb4799379c07
Hamzaali565 <ha6153806@gmail.com>
2022-11-28 15:43:35 +0500

    weather Updates

--- module.mjs
+++ module.mjs
@@ -2,13 +2,17 @@ import express from "express"
 import path from 'path'
 const app = express()
 const port = process.env.PORT||3000
-
-app.get('/abc', (req, res) => {
-  res.send('Hello World!')
-  console.log("request IP: ", req.ip );
-})
 const __dirname = path.resolve();
 app.use('/', express.static(path.join (__dirname, './web/build')))
+
+app.get('/weather', (req, res) => {
+  res.send({
+    temp: 30,
+    humidity: 90,
+    time: "11:30",
+  })
+  console.log("request IP: ", req.ip );
+})
 app.use('*', express.static(path.join (__dirname, './web/build')))
 
 
